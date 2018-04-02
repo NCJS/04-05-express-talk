d3a141e4d67531144a7e4167b79389fcbea95549
andreas.groos <andreas.groos1@gmail.com>
2018-04-02 12:41:16 -0700

    query & params, 404

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ require("dotenv").config();
 
 const app = express();
 
+const Logger = (req, res, next) => {
+  const query = req.query;
+  console.log("query: ", query);
+  next();
+};
+
+app.use(Logger);
+
 app.get("/", (req, res) => res.send("Hello World!"));
-app.get("/one", (req, res) => res.send("one endpoint"));
-app.get("/two", (req, res) => res.send("two endpoint "));
+app.get("/user/:username", (req, res) => {
+  const { params } = req;
+  console.log("params :", params);
+  res.status(200).json({ user: params.username });
+});
+app.get("*", (req, res) => {
+  res.status(404).send("No such thing");
+});
 
 const PORT = process.env.PORT || 4010;
 
