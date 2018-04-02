f782003cbda41c2255b8a7b48d872ddc0e46b353
andreas.groos <andreas.groos1@gmail.com>
2018-04-02 12:03:45 -0700

    most basic routing

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/one", (req, res) => res.send("one endpoint"));
+app.get("/two", (req, res) => res.send("two endpoint "));
 
 app.listen(4000, () => console.log("Example app listening on port 4000!"));
